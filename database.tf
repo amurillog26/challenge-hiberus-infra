897bd97d81d61c708aad1f012c571c391d92f79b
Arturo Murillo <arturomurillo@MacBook-Pro.local>
2024-05-27 10:07:07 -0600

    feat: add infra config and cicd workflow

---
+++ database.tf
@@ -0,0 +1,18 @@
+resource "digitalocean_database_cluster" "hiberus_db" {
+  name       = "hiberus-db"
+  engine     = "mysql"
+  version    = "8"
+  region     = "nyc1"
+  size       = "db-s-2vcpu-4gb"
+  node_count = 1
+}
+
+resource "digitalocean_database_user" "user_hiberus" {
+  cluster_id = digitalocean_database_cluster.hiberus_db.id
+  name       = "hiberus"
+}
+
+resource "digitalocean_database_db" "db_hiberus" {
+  cluster_id = digitalocean_database_cluster.hiberus_db.id
+  name       = "hiberus"
+}
---
+++ kubernetes.tf
@@ -0,0 +1,11 @@
+resource "digitalocean_kubernetes_cluster" "hiberus_cluster" {
+  name    = "hiberus-cluster"
+  region  = "nyc1"
+  version = "1.30.1-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-4gb"
+    node_count = 3
+  }
+}
