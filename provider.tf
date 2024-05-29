23ffd30f61f0ece2a5b53324b136619cb2ec3bb3
Arturo Murillo <arturomurillo@MacBook-Pro.local>
2024-05-29 13:05:37 -0600

    chore: delete infra

--- provider.tf
+++ provider.tf
@@ -1,20 +1,20 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "2.38.0"
-    }
-  }
-  backend "s3" {
-    endpoint   = "https://nyc3.digitaloceanspaces.com"
-    region     = "us-east-1"
-    bucket     = "challenge-hiberus-state"
-    key        = "terraform.tfstate"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-  }
-}
-
-# provider "digitalocean" {
-#   token = var.do_token
+# terraform {
+#   required_providers {
+#     digitalocean = {
+#       source  = "digitalocean/digitalocean"
+#       version = "2.38.0"
+#     }
+#   }
+#   backend "s3" {
+#     endpoint   = "https://nyc3.digitaloceanspaces.com"
+#     region     = "us-east-1"
+#     bucket     = "challenge-hiberus-state"
+#     key        = "terraform.tfstate"
+#     skip_credentials_validation = true
+#     skip_metadata_api_check     = true
+#   }
 # }
+
+# # provider "digitalocean" {
+# #   token = var.do_token
+# # }
